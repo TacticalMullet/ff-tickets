3d1421ce0b37a0f3dcf14d5aeece54643a929f09
Emmett <33905560+aridmeme@users.noreply.github.com>
2019-10-31 12:17:38 -0600

    pretty print fantasy football ticket lineup script

---
+++ README.md
@@ -0,0 +1,18 @@
+### Example Lineup
+```
+python gen_lineup.py --qb russ --dst 49ers --dst bills --k prater --rb cook --rb bell --wr cooper --wr golladay --wr hopkins --te kittle
+
+|  QB  |  DST  |   K    |  RB  |    WR    |   TE   |
+| russ | 49ers | prater | cook |  cooper  | kittle |
+| russ | 49ers | prater | cook | golladay | kittle |
+| russ | 49ers | prater | cook | hopkins  | kittle |
+| russ | 49ers | prater | bell |  cooper  | kittle |
+| russ | 49ers | prater | bell | golladay | kittle |
+| russ | 49ers | prater | bell | hopkins  | kittle |
+| russ | bills | prater | cook |  cooper  | kittle |
+| russ | bills | prater | cook | golladay | kittle |
+| russ | bills | prater | cook | hopkins  | kittle |
+| russ | bills | prater | bell |  cooper  | kittle |
+| russ | bills | prater | bell | golladay | kittle |
+| russ | bills | prater | bell | hopkins  | kittle |
+```
---
+++ gen_lineup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import argparse
+
+class Lineup:
+    def __init__(self, qb, dst, k, rb, wr, te):
+        self.qb = qb
+        self.dst = dst
+        self.k = k
+        self.rb = rb
+        self.wr = wr
+        self.te = te
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate Fantasy Ticket Lineups')
+    parser.add_argument('--qb', type=str, action='append')
+    parser.add_argument('--dst', type=str, action='append')
+    parser.add_argument('--k', type=str, action='append')
+    parser.add_argument('--rb', type=str, action='append')
+    parser.add_argument('--wr', type=str, action='append')
+    parser.add_argument('--te', type=str, action='append')
+
+    args = parser.parse_args()
+    qb = args.qb
+    dst = args.dst
+    k = args.k
+    rb = args.rb
+    wr = args.wr
+    te = args.te
+
+    lineups = [Lineup('QB', 'DST', 'K', 'RB', 'WR', 'TE')]
+
+    for a in qb:
+        for b in dst:
+            for c in k:
+                for d in rb:
+                    for e in wr:
+                        for f in te:
+                            lineups.append(Lineup(a,b,c,d,e,f))
+
+
+    qbl = '{{:^{}}}'.format(len(max(qb, key=len)))
+    dstl = '{{:^{}}}'.format(len(max(dst, key=len)))
+    kl = '{{:^{}}}'.format(len(max(k, key=len)))
+    rbl = '{{:^{}}}'.format(len(max(rb, key=len)))
+    wrl = '{{:^{}}}'.format(len(max(wr, key=len)))
+    tel = '{{:^{}}}'.format(len(max(te, key=len)))
+
+    fmt = '| {} | {} | {} | {} | {} | {} |'.format(qbl,dstl,kl,rbl,wrl,tel)
+
+    for l in lineups:
+        print(fmt.format(l.qb,l.dst,l.k,l.rb,l.wr,l.te))
